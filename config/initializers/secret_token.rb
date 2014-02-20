91667c61fc0f66b6c1094a0cff712dde54d7cb12
Isaac Chan <isaactinghong@gmail.com>
2014-02-20 18:51:43 +0800

    Initial commit friendery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Friendery::Application.config.secret_key_base = 'a26d5c86a13f24b6068ba9e73f17097bf74c6b21c5726dad0a4f7115cceaf7a65020c0972ad026d27a7c0793b63d973fade7b518a50fbb82f01329d5846ae0ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Friendery::Application.config.session_store :cookie_store, key: '_friendery_session'
